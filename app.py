a2a92fbeeaa7ffcb697fb454d33c73511a122755
SampathVinay2003 <100464051+SampathVinay2003@users.noreply.github.com>
2023-06-26 11:09:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def predict():
         else:
             Fuel_Type_Petrol=0
             Fuel_Type_Diesel=1
-        Year=2020-Year
+        Year=2023-Year
         Seller_Type_Individual=request.form['Seller_Type_Individual']
         if(Seller_Type_Individual=='Individual'):
             Seller_Type_Individual=1
